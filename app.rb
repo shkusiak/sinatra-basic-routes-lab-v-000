10c84d1a0ddf1610f40314ac44c961dbbfb5d3fb
Shea Kusiak <shkusiak@gmail.com>
2018-07-13 23:50:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Shea"
+  end
+
+  get '/hometown' do
+    "My hometown is Longmeadow."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is "
+  end
+
 end
